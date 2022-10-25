6e479015e33bf51fefc4066bc99e1e86913b649c
임빈영 <qlsdud960604@gmail.com>
2022-10-25 10:32:21 +0900

    Modify #3 - JdbcTemplate 클래스
    try-with-resource 구문을 활용해 자원을 자동으로 반납할 수 있도록 코드 간소화

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -10,43 +10,20 @@ import java.util.List;
 public class JdbcTemplate {
 
     public void update(String sql, PreparedStatementSetter pss) throws DataAccessException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+        try (Connection con = ConnectionManager.getConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql)) {
             pss.setValues(pstmt);
-
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new DataAccessException(e);
-        } finally {
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                    throw new DataAccessException(e);
-                }
-            }
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (SQLException e) {
-                    throw new DataAccessException(e);
-                }
-            }
         }
     }
 
     public List select(String sql, PreparedStatementSetter pss, RowMapper rm) throws DataAccessException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
         ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+        try (Connection con = ConnectionManager.getConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql)) {
             pss.setValues(pstmt);
-
             rs = pstmt.executeQuery();
 
             List<Object> result = new ArrayList<Object>();
@@ -59,26 +36,12 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             throw new DataAccessException(e);
         } finally {
-            if (rs != null) {
-                try {
+            try {
+                if (rs != null) {
                     rs.close();
-                } catch (SQLException e) {
-                    throw new DataAccessException(e);
                 }
-            }
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                    throw new DataAccessException(e);
-                }
-            }
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (SQLException e) {
-                    throw new DataAccessException(e);
-                }
+            } catch (SQLException e) {
+                throw new DataAccessException(e);
             }
         }
     }
