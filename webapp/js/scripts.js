9c11b11aab0e408360dca94a855d0f31fab222fa
임빈영 <qlsdud960604@gmail.com>
2022-10-31 11:00:50 +0900

    Modify #5 - script.js 파일
    댓글 삭제 기능을 위한 스크립트 작성

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -25,6 +25,30 @@ function onError(xhr, status) {
   alert("error");
 }
 
+$(".qna-comment").on("click", ".form-delete", deleteAnswer);
+
+function deleteAnswer(e) {
+  e.preventDefault();
+
+  var deleteBtn = $(this);
+  var queryString = deleteBtn.closest("form").serialize();
+
+  $.ajax({
+    type: 'post',
+    url: '/api/qna/deleteAnswer',
+    data: queryString,
+    dataType: 'json',
+    error: function (xhr, status) {
+      alert("erorr");
+    },
+    success: function (json, status) {
+      if (json.status) {
+        deleteBtn.closest('article').remove();
+      }
+    }
+  });
+}
+
 String.prototype.format = function() {
   var args = arguments;
   return this.replace(/{(\d+)}/g, function(match, number) {
