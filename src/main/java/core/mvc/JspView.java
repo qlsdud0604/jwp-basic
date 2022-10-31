b4ffb8b49986dd01d6266cc3809e201dde49989a
임빈영 <qlsdud960604@gmail.com>
2022-10-31 16:22:32 +0900

    Modify #7 - JspView, JsonView 클래스
    View 인터페이스 변경에 따라 JspView, JsonView 클래스 변경

--- src/main/java/core/mvc/JspView.java
+++ src/main/java/core/mvc/JspView.java
@@ -3,6 +3,8 @@ package core.mvc;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.Set;
 
 public class JspView implements View {
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
@@ -18,10 +20,15 @@ public class JspView implements View {
     }
 
     @Override
-    public void render(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public void render(Map<String, ?> model, HttpServletRequest req, HttpServletResponse resp) throws Exception {
         if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
             resp.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
             return;
+        }
+
+        Set<String> keys = model.keySet();
+        for (String key : keys) {
+            req.setAttribute(key, model.get(key));
         }
 
         RequestDispatcher rd = req.getRequestDispatcher(viewName);
