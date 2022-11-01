beea4f3607cc89932b45aef228dc343c90c799ca
임빈영 <qlsdud960604@gmail.com>
2022-11-01 16:45:32 +0900

    Modify #9 - DispatcherServlet 클래스
    두 개의 HandlerMapping이 모두 동작하도록 통합 작업

---
+++ src/main/java/core/nmvc/DispatcherServlet.java
@@ -0,0 +1,72 @@
+package core.nmvc;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.common.collect.Lists;
+import core.mvc.Controller;
+import core.mvc.LegacyHandlerMapping;
+import core.mvc.ModelAndView;
+import core.mvc.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
+public class DispatcherServlet extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
+    private List<HandlerMapping> mappings = Lists.newArrayList();
+
+    @Override
+    public void init() throws ServletException {
+        LegacyHandlerMapping lhm = new LegacyHandlerMapping();
+        lhm.initMapping();
+
+        AnnotationHandlerMapping ahm = new AnnotationHandlerMapping("next.controller");
+        ahm.initialize();
+
+        mappings.add(lhm);
+        mappings.add(ahm);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Object handler = getHandler(req);
+
+        if (handler == null) {
+            throw new IllegalArgumentException("존재하지 않는 URL입니다.");
+        }
+
+        try {
+            ModelAndView mav = execute(handler, req, resp);
+            View view = mav.getView();
+            view.render(mav.getModel(), req, resp);
+        } catch (Throwable e) {
+            logger.error("Exception : {}", e);
+            throw new ServletException(e.getMessage());
+        }
+    }
+
+    private Object getHandler(HttpServletRequest req) {
+        for (HandlerMapping handlerMapping : mappings) {
+            Object handler = handlerMapping.getHandler(req);
+            if (handler != null) {
+                return handler;
+            }
+        }
+        return null;
+    }
+
+    private ModelAndView execute(Object handler, HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        if (handler instanceof Controller) {
+            return ((Controller)handler).execute(req, resp);
+        } else {
+            return ((HandlerExecution)handler).handle(req, resp);
+        }
+    }
+}
