cfcb8b2bd7f4f8c08aca5837577bbd4e2e8922a3
임빈영 <qlsdud960604@gmail.com>
2022-11-01 13:56:21 +0900

    Add #9 - MyController 클래스
    ControllerScanner 클래스 테스트를 위한 MyController 클래스 생성

---
+++ src/main/java/core/mvc/MyController.java
@@ -0,0 +1,28 @@
+package core.mvc;
+
+import core.annotation.Controller;
+import core.annotation.RequestMapping;
+import core.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class MyController {
+    private static final Logger logger = LoggerFactory.getLogger(MyController.class);
+
+    @RequestMapping("/users/findUserId")
+    public ModelAndView findUserId(HttpServletRequest request, HttpServletResponse response) {
+        logger.debug("findUserId");
+        return null;
+    }
+
+    @RequestMapping(value = "/users/findUserId", method = RequestMethod.POST)
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        logger.debug("save");
+        return null;
+    }
+
+}
