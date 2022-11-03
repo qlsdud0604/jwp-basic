0a477110d10216762b6e7896802fcc99f801b089
임빈영 <qlsdud960604@gmail.com>
2022-11-03 15:18:44 +0900

    Add #11 - BeanFactory 클래스
    의존관계에 있는 각 클래스들의 빈 인스턴스를 생성하는 BeanFactory 클래스 생성

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,22 +1,28 @@
 package core.di.factory;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.google.common.collect.Lists;
+import core.annotation.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Maps;
+import org.springframework.beans.BeanUtils;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
 
-    private Set<Class<?>> preInstanticateBeans;
+    private Set<Class<?>> preInstantiateBeans;
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
 
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
+    public BeanFactory(Set<Class<?>> preInstantiateBeans) {
+        this.preInstantiateBeans = preInstantiateBeans;
     }
 
     @SuppressWarnings("unchecked")
@@ -25,6 +31,62 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class<?> clazz : preInstantiateBeans) {
+            if (beans.get(clazz) == null) {
+                instantiateClass(clazz);
+            }
+        }
+    }
 
+    /** Class에 대한 빈 인스턴스를 생성하는 메서드 */
+    private Object instantiateClass(Class<?> clazz) {
+        Object bean = beans.get(clazz);
+        if (bean != null) {
+            return bean;
+        }
+
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+
+        if (injectedConstructor == null) {
+            bean = BeanUtils.instantiate(clazz);
+            beans.put(clazz, bean);
+            return bean;
+        }
+
+        logger.debug("Constructor : {}", injectedConstructor);
+        bean = instantiateConstructor(injectedConstructor);
+        beans.put(clazz, bean);
+        return bean;
+    }
+
+    /** Constructor에 빈 인스턴스를 생성하는 메서드 */
+    private Object instantiateConstructor(Constructor<?> constructor) {
+        Class<?>[] pTypes= constructor.getParameterTypes();
+        List<Object> args = Lists.newArrayList();
+        for (Class<?> clazz : pTypes) {
+            Class<?> concreteClazz = BeanFactoryUtils.findConcreteClass(clazz, preInstantiateBeans);
+            if (!preInstantiateBeans.contains(concreteClazz)) {
+                throw new IllegalStateException(clazz + "는 Bean이 아니다.");
+            }
+
+            Object bean = beans.get(concreteClazz);
+            if (bean == null) {
+                bean = instantiateClass(concreteClazz);
+            }
+            args.add(bean);
+        }
+        return BeanUtils.instantiateClass(constructor, args.toArray());
+    }
+
+    /** @Controller 애너테이션이 설정된 클래스 정보를 반환 */
+    public Map<Class<?>, Object> getControllers() {
+        Map<Class<?>, Object> controllers = Maps.newHashMap();
+        for (Class<?> clazz : preInstantiateBeans) {
+            Annotation annotation = clazz.getAnnotation(Controller.class);
+            if (annotation != null) {
+                controllers.put(clazz, beans.get(clazz));
+            }
+        }
+        return controllers;
     }
 }
