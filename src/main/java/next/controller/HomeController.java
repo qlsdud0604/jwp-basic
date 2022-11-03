07cb6bffe64a6ec655a06510e2dbdccd0316d71e
임빈영 <qlsdud960604@gmail.com>
2022-11-03 15:07:39 +0900

    Modify #11 - HomeController, UserController 클래스
    싱글톤 패턴을 제거하고 DI 구조로 변경

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -3,19 +3,24 @@ package next.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import core.annotation.Controller;
+import core.annotation.Inject;
+import core.annotation.RequestMapping;
+import core.nmvc.AbstractNewController;
 import next.dao.QuestionDao;
-import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
 
-public class HomeController extends AbstractController {
+@Controller
+public class HomeController extends AbstractNewController {
     private QuestionDao questionDao;
 
+    @Inject
     public HomeController(QuestionDao questionDao) {
         this.questionDao = questionDao;
     }
 
-    @Override
-    public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @RequestMapping("/")
+    public ModelAndView home(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         return jspView("home.jsp").addObject("questions", questionDao.findAll());
     }
 }
