b9e91b9f2ceb52fe7b5fd0efd68c3c216148a91b
임빈영 <qlsdud960604@gmail.com>
2022-11-04 13:59:48 +0900

    Modify #13 - FieldInjector 클래스
    중복 코드를 제거하고 AbstractInjector 클래스를 상속받는 구조로 변경

--- src/main/java/core/di/factory/FieldInjector.java
+++ src/main/java/core/di/factory/FieldInjector.java
@@ -1,78 +1,38 @@
 package core.di.factory;
 
-import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.Set;
 
-public class FieldInjector implements Injector {
+public class FieldInjector extends AbstractInjector {
 
     private static final Logger log = LoggerFactory.getLogger(FieldInjector.class);
 
-    private BeanFactory beanFactory;
-
     public FieldInjector(BeanFactory beanFactory) {
-        this.beanFactory = beanFactory;
+        super(beanFactory);
     }
 
     @Override
-    public void inject(Class<?> clazz) {
-        instantiateClass(clazz);
-        Set<Field> injectedFields = BeanFactoryUtils.getInjectedFields(clazz);
-
-        for (Field field : injectedFields) {
-            Class<?> concreteClazz = BeanFactoryUtils.findConcreteClass(clazz, beanFactory.getPreInstantiateBeans());
-            Object bean = beanFactory.getBean(concreteClazz);
-            if (bean == null) {
-                bean = instantiateClass(concreteClazz);
-            }
-            try {
-                field.setAccessible(true);
-                field.set(beanFactory.getBean(field.getDeclaringClass()), bean);
-            } catch (IllegalAccessException | IllegalArgumentException e) {
-                log.error(e.getMessage());
-            }
-        }
+    Set<?> getInjectedBeans(Class clazz) {
+        return BeanFactoryUtils.getInjectedFields(clazz);
     }
 
-    /** Class에 대한 빈 인스턴스를 생성하는 메서드 */
-    private Object instantiateClass(Class<?> clazz) {
-        Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(clazz, beanFactory.getPreInstantiateBeans());
-        Object bean = beanFactory.getBean(concreteClass);
-        if (bean != null) {
-            return bean;
-        }
-
-        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(concreteClass);
-        if (injectedConstructor == null) {
-            bean = BeanUtils.instantiate(concreteClass);
-            beanFactory.registerBean(concreteClass, bean);
-            return bean;
-        }
-
-        log.debug("Constructor : {}", injectedConstructor);
-        bean = instantiateConstructor(injectedConstructor);
-        beanFactory.registerBean(concreteClass, bean);
-        return bean;
+    @Override
+    Class<?> getBeanClass(Object injectedBean) {
+        Field field = (Field)injectedBean;
+        return field.getType();
     }
 
-    /** Constructor에 대한 빈 인스턴스를 생성하는 메서드 */
-    private Object instantiateConstructor(Constructor<?> constructor) {
-        Class<?>[] pType = constructor.getParameterTypes();
-        List<Object> args = Lists.newArrayList();
-        for (Class<?> clazz : pType) {
-            Class<?> concreteClazz = BeanFactoryUtils.findConcreteClass(clazz, beanFactory.getPreInstantiateBeans());
-            Object bean = beanFactory.getBean(concreteClazz);
-            if (bean == null) {
-                bean = instantiateClass(concreteClazz);
-            }
-            args.add(bean);
+    @Override
+    void inject(Object injectedBean, Object bean, BeanFactory beanFactory) {
+        Field field = (Field)injectedBean;
+        try {
+            field.setAccessible(true);
+            field.set(beanFactory.getBean(field.getDeclaringClass()), bean);
+        } catch (IllegalAccessException | IllegalArgumentException e) {
+            log.error(e.getMessage());
         }
-        return BeanUtils.instantiateClass(constructor, args.toArray());
     }
 }
